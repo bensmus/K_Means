003025790bef774c0b66f4df259727889af53797
Ben Smus <bensmus@gmail.com>
2020-07-16 13:15:42 -0700

    one iter works

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def plot_points(points, color):
 def centroids_init(points, K):
     '''Choose K random points to be the initial centroids (around which to cluster).'''
     
-    centroids = [list(random.choice(points)) for _ in range(K)]
-    return centroids  # list with K points
+    centroids = np.array([random.choice(points) for _ in range(K)])
+    return centroids  # np.array with K points
 
 
 def find_closest_centroid(centroids, point):
@@ -35,30 +35,52 @@ def find_closest_centroid(centroids, point):
 
     distances = []
     for centroid in centroids:
-        distances.append(np.linalg.norm(np.array(centroid) - np.array(point)))
+        distances.append(np.linalg.norm(centroid - point))
     min_index = distances.index(min(distances))
     
     return centroids[min_index]
 
 
 def get_clusters(centroids, points):
-    '''returns an array of clusters (3D array)'''
+    '''returns an dict with values being clusters (2D numpy arrays)'''
     
-    clusters = [[] for _ in centroids]
+    cluster_dict = {i:np.zeros((1, 2)) for i in range(K)}
+
     for point in points:
-        print(centroids, clusters)
+        
         # deciding which cluster to assign to 
         centroid = find_closest_centroid(centroids, point)
-        cluster = clusters[centroids.index(centroid)]
-        cluster.append(point)
-    
-    return clusters
+        i = centroids.tolist().index(centroid.tolist())  # use the list.index(element) method
+        
+        comparison = cluster_dict[i] == np.zeros((1, 2))
+        equal_arrays = comparison.all()  # boolean values
+        if equal_arrays:
+            cluster_dict[i] = point.reshape(1, 2)  # making it 2D
+        else:
+            cluster_dict[i] = np.append(cluster_dict[i], point.reshape(1, 2), axis=0)
 
+    return cluster_dict
     
+
+def get_centroids(clusters):
+    '''returns an array of centroids (2D array)'''
+    
+    centroids = np.zeros((K, 2))
+    for i, cluster in enumerate(clusters.values()):
+        centroid = (np.cumsum(cluster, axis=0)[-1]) / cluster.shape[0]
+        centroids[i] = centroid
+    
+    return np.array(centroids)
+
+
 centroids = centroids_init(points, K)
 clusters = get_clusters(centroids, points)
+
+centroids_2 = get_centroids(clusters)
+
 for i in range(K):
-    plot_points(np.array(clusters[i]), coloring[i])
+    plot_points(clusters[i], coloring[i])
+plot_points(centroids_2, green)
 plt.show()
 '''
 for _ in range(count):
